17519293ca3eba2fa42d3bf07e2e2cc5f7f75559
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-21 21:37:47 -0400

    Add files via upload

---
+++ split_method_on_a_string.rb
@@ -0,0 +1,8 @@
+sentences = "Hi, my name is Naima. My favorite hobby is coding"
+
+p sentences.split 
+#split will give back to us array, after it has been split 
+
+p sentences.split(".")
+
+#. is the delimiter, ruby will split text here 
