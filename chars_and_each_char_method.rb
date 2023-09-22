5a9d0ecc5a2f91a52fa37156adc7e31435f6e083
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-21 21:43:17 -0400

    Add files via upload

---
+++ chars_and_each_char_method.rb
@@ -0,0 +1,11 @@
+vehicle = "Spaceship"
+
+characters = vehicle.split("")
+#this is saying split by every character 
+p characters 
+
+characters = vehicle.chars 
+p characters
+#this will also give an array of all characters of a string 
+#chars is short for characters 
+
