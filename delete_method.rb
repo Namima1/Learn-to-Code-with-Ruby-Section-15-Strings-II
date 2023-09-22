17957c3d5c5f04a239bbd502ea3108579cd40f52
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-21 22:01:56 -0400

    Add files via upload

---
+++ delete_method.rb
@@ -0,0 +1,8 @@
+#delete - remove characters from a string 
+
+p "cottage cheese".delete("c")
+
+#this deletes all Cs
+ p "cottage cheese".delete("ce")
+
+ #this deletes all c and e, order of the letters doesnt matter
