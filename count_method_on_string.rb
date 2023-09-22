24da3e5e18490bfd7766f1a214b65b85a339f0ee
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-21 21:51:17 -0400

    Add files via upload

---
+++ count_method_on_string.rb
@@ -0,0 +1,9 @@
+#count- counts the number of occurrences of characters is a string 
+
+puts "Hello World".count("l")
+
+#ruby counts the lowercase Ls 
+
+puts "Hello World".count("Wl")
+#ruby will count the number of upper case Ws and lowercase ls
+#the letters do not have to be next to each other to be counted 
