f8a9587eacc11550c3b521199ae1267dc3d248dc
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-21 21:47:15 -0400

    Add files via upload

---
+++ join_on_an_array.rb
@@ -0,0 +1,7 @@
+#join method-merge array elements into a string (optionally using a delimiter)
+
+registrants = ["Jake", "Mikey", "Hannah"]
+puts registrants.join 
+#join takes all array elements and makes them into one 
+puts registrants.join(" ")
+#this adds a space between strings
